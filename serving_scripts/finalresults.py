b9b1b2aff7b32f2e5c95338a98a6470d81af89fb
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-04-29 15:44:59 -0600

    Update finalresults.py

--- serving_scripts/finalresults.py
+++ serving_scripts/finalresults.py
@@ -1,10 +1,34 @@
 import sys
 
 if len(sys.argv) == 1:
-  print 'No args, really??'
+  conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+  cur = conn.cursor()
+  
+  #Query - No args
+  cur.execute("SELECT word, count FROM Tweetwordcount ORDER BY word, count")
+  records = cur.fetchall()
+  for rec in records:
+  print "word = ", rec[0]
+  print "count = ", rec[1], "\n"
+  
+  conn.commit()
+  conn.close()
 
 elif len(sys.argv) == 2:
   print str(sys.argv[1])
+  
+  conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+  cur = conn.cursor()
+  
+  #Query - arg
+  cur.execute("SELECT word, count FROM Tweetwordcount WHERE word=%s", word)
+  records = cur.fetchall()
+  for rec in records:
+  print "word = ", rec[0]
+  print "count = ", rec[1], "\n"
+  
+  conn.commit()
+  conn.close()
 
 else:
   print 'Woow there cowboy, please limit yourself to just one argument at a time!'
