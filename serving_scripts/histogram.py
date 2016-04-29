19c031d9ef4663c6dc449210d13a3471f1851fe6
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-04-29 16:41:46 -0600

    Update histogram.py

--- serving_scripts/histogram.py
+++ serving_scripts/histogram.py
@@ -10,16 +10,15 @@ elif len(sys.argv) == 2:
   range_split = string.split(range,',')
   low= range_split[0]
   high = range_split[1]
-  print low, high
   
   conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
   cur = conn.cursor()
   
   #Query - arg
-  cur.execute("SELECT word, count FROM Tweetwordcount WHERE word=%s",[word])
+  cur.execute("SELECT word, count FROM Tweetwordcount WHERE count >= %d AND count <= %d ORDER BY word, count", (low,high))
   records = cur.fetchall()
   for rec in records:
-    print "Total number of occurences of '%s': %d" %(rec[0], rec[1]), "\n"
+    print "(%s, %d)" %(rec[0], rec[1]), "\n"
   
   conn.commit()
   conn.close()
