092395103b857c3ad79b0e1ee085df9aff828086
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-04-29 15:23:04 -0600

    Create finalresults.py

---
+++ serving_scripts/finalresults.py
@@ -0,0 +1,10 @@
+import sys
+
+if len(sys.argv) == 0:
+  print 'No args, really??'
+
+elif len(sys.argv) == 1:
+  print str(sys.argv)
+
+else:
+  print 'Woow there cowboy, please limit yourself to just one argument at a time!'
