64d55b56ff818d1a35858a2e132486ca745938e4
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-04-29 15:25:18 -0600

    Update finalresults.py

--- serving_scripts/finalresults.py
+++ serving_scripts/finalresults.py
@@ -1,10 +1,10 @@
 import sys
 
-if len(sys.argv) == 0:
+if len(sys.argv) == 1:
   print 'No args, really??'
 
-elif len(sys.argv) == 1:
-  print str(sys.argv)
+elif len(sys.argv) == 2:
+  print str(sys.argv[2])
 
 else:
   print 'Woow there cowboy, please limit yourself to just one argument at a time!'
